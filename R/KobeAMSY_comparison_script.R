a95b32babc80ba688007d4552ea3f74c172f808e
framasnadi <francesco.masnadi@irbim.cnr.it>
2021-11-04 15:10:46 +0100

    add SOL e MTS with traject

--- R/KobeAMSY_comparison_script.R
+++ R/KobeAMSY_comparison_script.R
@@ -3,14 +3,14 @@ library(readr)
 library(ss3diags)
 library(dplyr)
 
-out_SOL <- read_csv(paste0("AMSY/Out_man/Out_novembre012021_SOL_ID.csv"))
-out_MTS <- read_csv(paste0("AMSY/Out_man/Out_novembre012021_MTS_ID.csv"))
+out_SOL <- read_csv(paste0("AMSY/Out_man/Out_novembre042021_SOL_ID.csv"))
+out_MTS <- read_csv(paste0("AMSY/Out_man/Out_novembre042021_MTS_ID.csv"))
 out_CTC <- read_delim("AMSY/Out_man/Out_ottobre262021_CTC_ID.csv", 
                       ";", escape_double = FALSE, trim_ws = TRUE)
 out_SJA <- read_csv(paste0("AMSY/Out_man/Out_ottobre262021_SJA_ID.csv"))
 
-species <- "CTC"  
-out <- out_CTC # put the output of species of interest
+species <- "MTS"  
+out <- out_MTS # put the output of species of interest
 
 methods=c('Trust', 'diva','prop','ssa','pred')
 amsy_kobe=NULL
